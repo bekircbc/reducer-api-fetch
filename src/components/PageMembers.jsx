741e53ce0f88c505beb5e70f95e651dce3c906e5
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-17 14:55:57 +0200

    PageMembers created

---
+++ src/components/PageMembers.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { AppContext } from "../AppContext";
+
+export const PageMembers = () => {
+  const { members } = useContext(AppContext);
+
+  return (
+    <>
+      <h2>Members</h2>
+      <p>The following people are our members:</p>
+      <ul>
+        {members.map((member, index) => {
+          return (
+            <li key={index}>
+              {member.firstName} {member.lastName}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
